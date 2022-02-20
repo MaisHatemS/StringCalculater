d9e394595883a8c6ddafd05791037ea4f5fac4ba
maissuleiman <maissuleiman@hotmail.com>
2022-02-20 16:52:37 +0200

    Add project files.

---
+++ StringCalculaterModule.fs
@@ -0,0 +1,14 @@
+﻿module StringCalculaterModule
+
+open System 
+
+
+type StringCalculator()=
+     member x.add expression =
+      match expression with 
+      |"" -> 0
+      |_ when expression.Contains ',' ->
+           let numbers=expression.Split[|','|]
+           (Int32.Parse numbers.[0])+(Int32.Parse numbers.[1])
+      |_-> Int32.Parse expression
+
---
+++ UnitTest1.fs
@@ -0,0 +1,27 @@
+module StringCalculater
+
+open NUnit.Framework
+open StringCalculaterModule
+[<SetUp>]
+let Setup () =
+    ()
+
+
+[<TestFixture>]
+type stringCalculatr()=
+    [<Test>]
+    member x.AddEmptyString_ReturnsZero () = 
+      let calc = StringCalculator() 
+      Assert.That(calc.add "",Is.EqualTo 0)
+
+    [<TestCase("1",ExpectedResult = 1)>]
+    [<TestCase("2",ExpectedResult = 2)>]
+    member x.Add_SingleNumber_ReturnsThatNumber expression=
+      let calc= StringCalculator()
+      calc.add expression
+
+    [<TestCase("1,3",ExpectedResult = 4)>]
+    member x.Add_TwoNumber_ReturnTheSumation expression=
+        let calc=StringCalculator()
+        calc.add expression
+   
